a544bdf223ce02b0c72d431ce2ae9271debff5a5
Julia Gerlach <juliagerlach442@gmail.com>
2018-06-26 15:57:08 -0500

    added HumaneSocietyDataContext fields in query class

--- HumaneSociety/Query.cs
+++ HumaneSociety/Query.cs
@@ -10,140 +10,224 @@ namespace HumaneSociety
     {
         internal static Client GetClient(string userName, string password)
         {
+            HumaneSocietyDataContext database = new HumaneSocietyDataContext();
+
+            database.SubmitChanges();
             throw new NotImplementedException();
         }
 
         internal static void RunEmployeeQueries(Employee employee, string v)
         {
+            HumaneSocietyDataContext database = new HumaneSocietyDataContext();
+
+            database.SubmitChanges();
             throw new NotImplementedException();
         }
 
         internal static IQueryable<ClientAnimalJunction> GetUserAdoptionStatus(Client client)
         {
+            HumaneSocietyDataContext database = new HumaneSocietyDataContext();
+
+            database.SubmitChanges();
             throw new NotImplementedException();
         }
 
         internal static Animal GetAnimalByID(int iD)
         {
+            HumaneSocietyDataContext database = new HumaneSocietyDataContext();
+
+            database.SubmitChanges();
             throw new NotImplementedException();
         }
 
         internal static void Adopt(object animal, Client client)
         {
+            HumaneSocietyDataContext database = new HumaneSocietyDataContext();
+
+            database.SubmitChanges();
             throw new NotImplementedException();
         }
 
         internal static IQueryable<Client> RetrieveClients()
         {
+            HumaneSocietyDataContext database = new HumaneSocietyDataContext();
+
+            database.SubmitChanges();
             throw new NotImplementedException();
         }
 
         internal static void AddAnimal(Animal animal)
         {
+            HumaneSocietyDataContext database = new HumaneSocietyDataContext();
+
+            database.SubmitChanges();
             throw new NotImplementedException();
         }
 
         internal static void AddNewClient(string firstName, string lastName, string username, string password, string email, string streetAddress, int zipCode, int state)
         {
+            HumaneSocietyDataContext database = new HumaneSocietyDataContext();
+
+            database.SubmitChanges();
             throw new NotImplementedException();
         }
 
         internal static void AddUsernameAndPassword(Employee employee)
         {
+            HumaneSocietyDataContext database = new HumaneSocietyDataContext();
+
+            database.SubmitChanges();
             throw new NotImplementedException();
         }
 
         internal static bool CheckEmployeeUserNameExist(string username)
         {
+            HumaneSocietyDataContext database = new HumaneSocietyDataContext();
+
+            database.SubmitChanges();
             throw new NotImplementedException();
         }
 
         internal static Employee EmployeeLogin(string userName, string password)
         {
+            HumaneSocietyDataContext database = new HumaneSocietyDataContext();
+
+            database.SubmitChanges();
             throw new NotImplementedException();
         }
 
         internal static void EnterUpdate(Animal animal, Dictionary<int, string> updates)
         {
+            HumaneSocietyDataContext database = new HumaneSocietyDataContext();
+
+            database.SubmitChanges();
             throw new NotImplementedException();
         }
 
         internal static int? GetBreed()
         {
+            HumaneSocietyDataContext database = new HumaneSocietyDataContext();
+
+            database.SubmitChanges();
             throw new NotImplementedException();
         }
 
         internal static IQueryable<ClientAnimalJunction> GetPendingAdoptions()
         {
+            HumaneSocietyDataContext database = new HumaneSocietyDataContext();
+
+            database.SubmitChanges();
             throw new NotImplementedException();
         }
 
         internal static int? GetDiet()
         {
+            HumaneSocietyDataContext database = new HumaneSocietyDataContext();
+
+            database.SubmitChanges();
             throw new NotImplementedException();
         }
 
         internal static int? GetLocation()
         {
+            HumaneSocietyDataContext database = new HumaneSocietyDataContext();
+
+            database.SubmitChanges();
             throw new NotImplementedException();
         }
 
         internal static IQueryable<AnimalShotJunction> GetShots(Animal animal)
         {
+            HumaneSocietyDataContext database = new HumaneSocietyDataContext();
+
+            database.SubmitChanges();
             throw new NotImplementedException();
         }
 
         internal static IQueryable<USState> GetStates()
         {
+            HumaneSocietyDataContext database = new HumaneSocietyDataContext();
+
+            database.SubmitChanges();
             throw new NotImplementedException();
         }
 
         internal static void RemoveAnimal(Animal animal)
         {
+            HumaneSocietyDataContext database = new HumaneSocietyDataContext();
+
+            database.SubmitChanges();
             throw new NotImplementedException();
         }
 
         internal static Employee RetrieveEmployeeUser(string email, int employeeNumber)
         {
+            HumaneSocietyDataContext database = new HumaneSocietyDataContext();
+
+            database.SubmitChanges();
             throw new NotImplementedException();
         }
 
         internal static void UpdateAddress(Client client)
         {
+            HumaneSocietyDataContext database = new HumaneSocietyDataContext();
+
+            database.SubmitChanges();
             throw new NotImplementedException();
         }
 
         internal static void UpdateAdoption(bool v, ClientAnimalJunction clientAnimalJunction)
         {
+            HumaneSocietyDataContext database = new HumaneSocietyDataContext();
+
+            database.SubmitChanges();
             throw new NotImplementedException();
         }
 
         internal static void updateClient(Client client)
         {
+            HumaneSocietyDataContext database = new HumaneSocietyDataContext();
+
+            database.SubmitChanges();
             throw new NotImplementedException();
         }
         internal static void UpdateShot(string v, Animal animal)
         {
+            HumaneSocietyDataContext database = new HumaneSocietyDataContext();
+
+            database.SubmitChanges();
             throw new NotImplementedException();
         }
 
         internal static void UpdateUsername(Client client)
         {
+            HumaneSocietyDataContext database = new HumaneSocietyDataContext();
+
+            database.SubmitChanges();
             throw new NotImplementedException();
         }
 
         internal static void UpdateLastName(Client client)
         {
+            HumaneSocietyDataContext database = new HumaneSocietyDataContext();
+
+            database.SubmitChanges();
             throw new NotImplementedException();
         }
 
         internal static void UpdateFirstName(Client client)
         {
+            HumaneSocietyDataContext database = new HumaneSocietyDataContext();
+
+            database.SubmitChanges();
             throw new NotImplementedException();
         }
 
         internal static void UpdateEmail(Client client)
         {
+            HumaneSocietyDataContext database = new HumaneSocietyDataContext();
+
+            database.SubmitChanges();
             throw new NotImplementedException();
         }
 
